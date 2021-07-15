8fc4c2001fb3917107329818e9a5cd7db1d8a50b
Christopher Plain <me@christopherplain.com>
2021-07-14 21:14:19 -0700

    Initial Commit

---
+++ Module4WrapUpChallenge/Module4WrapUpChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Module4WrapUpChallengeApp.swift
+//  Module4WrapUpChallenge
+//
+//  Created by Christopher Plain on 7/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Module4WrapUpChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
